6dd4eecb42f120c9ab9e36ed703733c573e31f14
Samantha Karas <samanthakaras1@gmail.com>
2023-03-22 14:21:32 -0400

    update

--- server.js
+++ server.js
@@ -11,6 +11,25 @@ app.get('/greeting/:sally', (req, res) => {
     res.send('Wow, hello there, Sally!')
 });
 
+// tip calculator
+
+app.get('/tip', (req,res) => {
+    res.send()
+})
+
+app.get('/tip/:total', (req,res) => {
+    res.send()
+})
+
+app.get('/tip/:tipPercentage', (req,res) => {
+    res.send()
+})
+
+// magic 8 ball
+
+
+// pass it around
+
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
   });
