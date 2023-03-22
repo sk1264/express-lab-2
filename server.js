33974ad00631c51964690204e922885512425e06
Samantha Karas <samanthakaras1@gmail.com>
2023-03-22 14:37:22 -0400

    greeting

--- server.js
+++ server.js
@@ -7,25 +7,24 @@ app.get('/greeting', (req, res) => {
     res.send('Hello, stranger!');
   });
 
-app.get('/greeting/:sally', (req, res) => {
-    res.send('Wow, hello there, Sally!')
+app.get('/greeting/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Wow, hello there, ${name}!`);
 });
 
 // tip calculator
 
-app.get('/tip', (req,res) => {
-    res.send()
+app.get('/tip/:total/:tipPercentage', (req,res) => {
 })
 
-app.get('/tip/:total', (req,res) => {
-    res.send()
-})
-
-app.get('/tip/:tipPercentage', (req,res) => {
-    res.send()
-})
 
 // magic 8 ball
+
+// app.get('/magic', (req, res) => {
+//     res.send('');
+//   });
+
+//   ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
 
 // pass it around
