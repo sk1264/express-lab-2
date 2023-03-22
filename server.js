75bef7c1494ac94bd3090c2b1830187a5a21f63b
Samantha Karas <samanthakaras1@gmail.com>
2023-03-22 15:00:35 -0400

    update

--- server.js
+++ server.js
@@ -15,17 +15,22 @@ app.get('/greeting/:name', (req, res) => {
 // tip calculator
 
 app.get('/tip/:total/:tipPercentage', (req,res) => {
+    const total = Number(req.params.total);
+    const tipPercentage = Number(req.params.tipPercentage);
+    const tipAmount = (total * tipPercentage) / 100;
+    res.send(`${tipAmount}`);
 })
-
 
 // magic 8 ball
 
-// app.get('/magic', (req, res) => {
-//     res.send('');
-//   });
+magicResponses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
-//   ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
+app.get('/magic/:question', (req, res) => {
+    const question = req.params.question;
+    const response = magicResponses[Math.floor(Math.random() * magicResponses.length)];
+    res.send(`<h1> Ask the Magic 8 Ball: ${question} </h1> <h1>Your response: ${response}</h1>`);
+  });
 
 // pass it around
 
